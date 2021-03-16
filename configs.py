7db2ba2bb3275441c83969e3a7547afb933c1334
Dipanjan <Dipanjan>
2021-03-16 21:20:48 +0530

    Reverted secret key

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import urandom
 
 
 # Application Secret Key
-app.config['SECRET_KEY'] = urandom(32)
+app.config['SECRET_KEY'] = "secret"
 
 # Login Credentials
 app.config['CREDENTIALS_USERNAME'] = 'admin'
